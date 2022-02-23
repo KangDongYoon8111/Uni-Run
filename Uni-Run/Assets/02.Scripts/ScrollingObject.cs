a03242d6f4a53cb63dc4fecb0738c886cd1142cf
강동윤 <davidkang8111@gmail.com>
2022-02-23 12:39:59 +0900

    20220223

---
+++ Uni-Run/Assets/02.Scripts/ScrollingObject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float speed = 10f; // 이동속도
+
+    void Update()
+    {
+        // 초당 speed의 속도로 왼쪽으로 평행이동
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //transform.Translate((transform.right * -1 )* speed * Time.deltaTime);
+    }
+}
