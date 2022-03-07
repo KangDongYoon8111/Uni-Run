e94c96c4595124b6386fcddd263cb5f1a71a2442
강동윤 <davidkang8111@gmail.com>
2022-03-07 12:40:50 +0900

    20220307

--- Uni-Run/Assets/02.Scripts/PlatformSpawner.cs
+++ Uni-Run/Assets/02.Scripts/PlatformSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 // 발판을 생성하고 주기적으로 재배치하는 스크립트
 public class PlatformSpawner : MonoBehaviour
 {
+    float deltaTest;
+    float timeTest;
     // 생성할 발판의 원본 프리팹
     public GameObject platformPrefab;
     // 생성할 발판 수
@@ -63,5 +65,34 @@ public class PlatformSpawner : MonoBehaviour
 
         // 마지막 배치 시점에서 timeBetSpawn 이상 시간이
         // 흘렀다면...
+        if(Time.time >= lastSpawnTime + timeBetSpawn)
+        {
+            // 기록된 마지막 배치 시점을 현재 시점으로 갱신
+            lastSpawnTime = Time.time;
+
+            // 다음 배치까지의 시간 간격을 timeBetSpawnMin, timeBetSpawnMax 사이에서 랜덤 설정
+            timeBetSpawn = Random.Range(timeBetSpawnMin, 
+                timeBetSpawnMax);
+
+            // 배치할 위치의 높이를 yMin과 yMax 사이에서 랜덤 설정
+            float yPos = Random.Range(yMin, yMax);
+
+            // 사용할 현재 순번의 발판 게임 오브젝트를 비활성화하고 즉시 다시 활성화
+            // 이 때 발판의 Platform 컴포넌트의 OnEnable 메서드가 실행됨
+            platforms[currentIndex].SetActive(false);
+            platforms[currentIndex].SetActive(true);
+
+            // 현재 순번의 발판을 화면 오른쪽에 재배치
+            platforms[currentIndex].transform.position = 
+                new Vector2(xPos, yPos);
+            // 순번 넘기기
+            currentIndex++;
+
+            // 마지막 순번에 도달했다면 순번을 리셋
+            if(currentIndex >= count)
+            {
+                currentIndex = 0;
+            }
+        }
     }
 }
